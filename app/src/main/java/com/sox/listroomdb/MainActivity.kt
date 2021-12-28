99ad688aa1efbfec1e708b33cf457f76812e1423
pchunter97 <jchariguaman@gmail.com>
2021-12-28 06:27:45 -0500

    Initial commit

---
+++ app/src/main/java/com/sox/listroomdb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sox.listroomdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
